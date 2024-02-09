3eacd921c1fe57cce38cf0ea88d5a9f01e0e31b7
breath_of_happiness <148492571+breath-of-happiness@users.noreply.github.com>
2024-02-09 16:46:16 +0300

    Add files via upload

---
+++ Лабораторная работа №3/task_1.py
@@ -0,0 +1,58 @@
+class Book:
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+    @property
+    def name(self):
+        return self._name
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+    @property
+    def pages(self) -> int:
+        return self._pages
+    @pages.setter
+    def pages(self, new_pages: int) -> None:
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Длительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+    @property
+    def duration(self) -> float:
+        return self._duration
+    @duration.setter
+    def duration(self, new_duration: float) -> None:
+        if not isinstance(new_duration, float):
+            raise TypeError("Длительность должна быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Длительность должна быть положительным числом")
+        self._duration = new_duration
